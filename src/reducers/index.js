949ac32c31eeb9583ab33a1e1a50cce46cd32633
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-09-11 20:07:33 +0530

    feat : created reducers to return states or functions based on the actions using the thunk middleware

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import tweets from './tweets'
+import authedUser from './authedUser'
+import users from './users'
+
+export default combineReducers({
+    tweets,
+    authedUser,
+    users
+})
