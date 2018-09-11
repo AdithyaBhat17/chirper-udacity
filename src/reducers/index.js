f7984960e7464135cefe265acd3cf7d85799eeab
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-09-11 21:12:59 +0530

    Feat : Added LoadingBar feature

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import tweets from './tweets'
 import authedUser from './authedUser'
 import users from './users'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
     tweets,
     authedUser,
-    users
+    users,
+    loadingBar : loadingBarReducer
 })
