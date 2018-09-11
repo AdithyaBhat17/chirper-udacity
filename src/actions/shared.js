5751529c272969a46df2b2a40a91f659fbc09c02
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-09-11 21:11:59 +0530

    Feat : Added showloading and hideloading features from the react-redux-loading package

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,18 +2,21 @@ import { getInitialData } from '../utils/api'
 import { receiveTweets } from './tweets'
 import { receiveUsers } from './users'
 import { setAuthedUser } from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = "adithya_nr"
 
 //using redux thunk middleware to return a function to request initial data from the api.
 export default function handleInitialData(){
     return (dispatch) => {
+        dispatch(showLoading())
         //get users & tweets and save them as states in our redux store.
         return getInitialData()
         .then(( { users,tweets } ) => {
             dispatch(receiveTweets(tweets))
             dispatch(receiveUsers(users))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
         })
     }
 }
